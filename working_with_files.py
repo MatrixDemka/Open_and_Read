5c9d2a0764022554eff76b86820d06cec5f756a9
Aleksey Demin <matrixdemka@yandex.ru>
2024-10-09 23:25:02 +0300

    in work

--- working_with_files.py
+++ working_with_files.py
@@ -1,5 +1,7 @@
 import pprint as pprint
 
+
+#Task1
 def read_recipes(recipe):
     """
     Функция считывает рецепты из внешнего текстового файла
@@ -25,6 +27,7 @@ def read_recipes(recipe):
     return cook_book
 
 
+#Task2
 def get_shop_list_by_dishes(dishes, person_count):
     """
     Функция возвращает словарь с названием ингредиентов и их количеством для блюд на заданное число персон
@@ -51,9 +54,32 @@ def get_shop_list_by_dishes(dishes, person_count):
     return quantity_ingredients
 
 
-
 #pprint.pprint(cook_book)
 cook_book = "recipes.txt"
 read_recipes(cook_book)
 
-pprint.pprint(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
+#pprint.pprint(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
+
+
+#Task3
+with open ("1.txt", encoding="utf-8") as file_one, open("2.txt", encoding="utf-8") as file_two, open("3.txt", encoding="utf-8") as file_three:
+    all_txt_files = {}
+
+    ffile_1 = (file_one.readlines())
+    ffile_2 = (file_two.readlines())
+    ffile_3 = (file_three.readlines())
+
+    all_txt_files[len(ffile_1)] = ["1.txt", ffile_1]
+    all_txt_files[len(ffile_2)] = ["2.txt", ffile_2]
+    all_txt_files[len(ffile_3)] = ["3.txt", ffile_3]
+
+#print(sorted(all_txt_files.items()))
+#print(all_txt_files[9][1])
+
+with open ("result.txt", "w", encoding="utf-8") as res:
+    for i in sorted(all_txt_files):
+        for a in all_txt_files[i]:
+            res.write(str(all_txt_files[a][0]))
+                    #"\n", i"\n", all_txt_files[i][1])
+with open ("result.txt", encoding="utf-8") as f:
+    print(f.read())
