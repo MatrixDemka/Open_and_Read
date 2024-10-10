b0d5bc0fce352a1883616e29f9be95430e5c6dc7
Aleksey Demin <matrixdemka@yandex.ru>
2024-10-10 18:20:47 +0300

    added Task3

--- working_with_files.py
+++ working_with_files.py
@@ -64,22 +64,24 @@ read_recipes(cook_book)
 #Task3
 with open ("1.txt", encoding="utf-8") as file_one, open("2.txt", encoding="utf-8") as file_two, open("3.txt", encoding="utf-8") as file_three:
     all_txt_files = {}
-
+    #Поочерёдное чтеие файлов
     ffile_1 = (file_one.readlines())
     ffile_2 = (file_two.readlines())
     ffile_3 = (file_three.readlines())
-
+    #Создаём словарь с ключи колличество строк в файле, значение словарь [имя файла, [строки текста]]
     all_txt_files[len(ffile_1)] = ["1.txt", ffile_1]
     all_txt_files[len(ffile_2)] = ["2.txt", ffile_2]
     all_txt_files[len(ffile_3)] = ["3.txt", ffile_3]
+    print(all_txt_files)
 
-#print(sorted(all_txt_files.items()))
-#print(all_txt_files[9][1])
 
 with open ("result.txt", "w", encoding="utf-8") as res:
-    for i in sorted(all_txt_files):
-        for a in all_txt_files[i]:
-            res.write(str(all_txt_files[a][0]))
-                    #"\n", i"\n", all_txt_files[i][1])
+    for key in sorted(all_txt_files):
+        res.write(str(all_txt_files[key][0]) + "\n")
+        res.write(str(key) + "\n")
+        res.writelines(all_txt_files[key][1])
+        #res.write("\n")
+
+
 with open ("result.txt", encoding="utf-8") as f:
     print(f.read())
